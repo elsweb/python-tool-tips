a24980eea34498bfffe6b1ad67357d647065d9fc
Emanuel Lds <toolsjob.neodeon@gmail.com>
2023-02-28 08:58:39 -0300

    playground - talk

--- play.py
+++ play.py
@@ -1,9 +1,27 @@
 import array
 
-lines = array.array('s',[])
+lines = []
+skey = 'Heliene'
+
+me = ""
+contato = ""
 
 with open("chat.txt", "r", encoding="utf8") as f:
     for row in f:
-        lines.append(row)
+        b = row.split(': ')
+        key = b[0].split('-')[1].strip()
+        if key == skey:
+            contato = contato +" "+ b[1].strip()
+            if(me != ""):
+                lines.append(me.strip().lower())
+                me = "" 
+        else:
+            me = me +" "+ b[1].strip()
+            if(contato != ""):
+                lines.append(contato.strip().lower())
+                contato = ""
+            
+        # break
+        # lines.append()
 
 print(lines)
